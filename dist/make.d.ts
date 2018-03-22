439c3100141b7a35b5be642e9b7031137a60f927
nivinjoseph <nivinjoseph@outlook.com>
2018-03-22 06:48:57 -0400

    preparing to publish new version

--- dist/make.d.ts
+++ dist/make.d.ts
@@ -1,8 +1,8 @@
 export declare abstract class Make {
     private constructor();
-    static retry<T>(func: (...params: any[]) => Promise<T>, numberOfRetries: number, errorPredicate?: (err: Error) => boolean): (...params: any[]) => Promise<T>;
-    static retryWithDelay<T>(func: (...params: any[]) => Promise<T>, numberOfRetries: number, delayMS: number, errorPredicate?: (err: Error) => boolean): (...params: any[]) => Promise<T>;
-    static retryWithExponentialBackoff<T>(func: (...params: any[]) => Promise<T>, numberOfRetries: number, errorPredicate?: (err: Error) => boolean): (...params: any[]) => Promise<T>;
+    static retry<T>(func: (...params: any[]) => Promise<T>, numberOfRetries: number, errorPredicate?: (error: any) => boolean): (...params: any[]) => Promise<T>;
+    static retryWithDelay<T>(func: (...params: any[]) => Promise<T>, numberOfRetries: number, delayMS: number, errorPredicate?: (error: any) => boolean): (...params: any[]) => Promise<T>;
+    static retryWithExponentialBackoff<T>(func: (...params: any[]) => Promise<T>, numberOfRetries: number, errorPredicate?: (error: any) => boolean): (...params: any[]) => Promise<T>;
     static syncToAsync<T>(func: (...params: any[]) => T): (...params: any[]) => Promise<T>;
     static callbackToPromise<T>(func: (...params: any[]) => void): (...params: any[]) => Promise<T>;
     private static getRandomInt(min, max);
