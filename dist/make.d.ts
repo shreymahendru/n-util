62144df0c603b1514cda8e3d2cc3cafaaa73ddc0
nivinjoseph <nivinjoseph@outlook.com>
2018-04-19 19:27:52 -0400

    preparing to publish new version

--- dist/make.d.ts
+++ dist/make.d.ts
@@ -5,5 +5,7 @@ export declare abstract class Make {
     static retryWithExponentialBackoff<T>(func: (...params: any[]) => Promise<T>, numberOfRetries: number, errorPredicate?: (error: any) => boolean): (...params: any[]) => Promise<T>;
     static syncToAsync<T>(func: (...params: any[]) => T): (...params: any[]) => Promise<T>;
     static callbackToPromise<T>(func: (...params: any[]) => void): (...params: any[]) => Promise<T>;
+    static loop(func: () => void, numberOfTimes: number): void;
+    static loopAsync(asyncFunc: () => Promise<void>, numberOfTimes: number, degreesOfParallelism?: number): Promise<void>;
     private static getRandomInt(min, max);
 }
--- dist/make.js
+++ dist/make.js
@@ -149,6 +149,20 @@ class Make // static class
         };
         return result;
     }
+    static loop(func, numberOfTimes) {
+        n_defensive_1.given(func, "func").ensureHasValue().ensureIsFunction();
+        n_defensive_1.given(numberOfTimes, "numberOfTimes").ensureHasValue().ensureIsNumber().ensure(t => t > 0);
+        for (let i = 0; i < numberOfTimes; i++)
+            func();
+    }
+    static loopAsync(asyncFunc, numberOfTimes, degreesOfParallelism) {
+        return __awaiter(this, void 0, void 0, function* () {
+            n_defensive_1.given(asyncFunc, "asyncFunc").ensureHasValue().ensureIsFunction();
+            n_defensive_1.given(numberOfTimes, "numberOfTimes").ensureHasValue().ensureIsNumber().ensure(t => t > 0);
+            let taskManager = new TaskManager(numberOfTimes, asyncFunc, degreesOfParallelism, false);
+            yield taskManager.execute();
+        });
+    }
     static getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
@@ -156,4 +170,72 @@ class Make // static class
     }
 }
 exports.Make = Make;
+class TaskManager {
+    constructor(numberOfTimes, taskFunc, taskCount, captureResults) {
+        this._numberOfTimes = numberOfTimes;
+        this._taskFunc = taskFunc;
+        this._taskCount = !taskCount || taskCount <= 0 ? numberOfTimes : taskCount;
+        this._captureResults = captureResults;
+        this._tasks = [];
+        for (let i = 0; i < this._taskCount; i++)
+            this._tasks.push(new Task(this, i, this._taskFunc, captureResults));
+        if (this._captureResults)
+            this._results = [];
+    }
+    execute() {
+        return __awaiter(this, void 0, void 0, function* () {
+            for (let i = 0; i < this._numberOfTimes; i++) {
+                if (this._captureResults)
+                    this._results.push(null);
+                yield this.executeTaskForItem(null, i);
+            }
+            yield this.finish();
+        });
+    }
+    addResult(itemIndex, result) {
+        this._results[itemIndex] = result;
+    }
+    getResults() {
+        return this._results;
+    }
+    executeTaskForItem(item, itemIndex) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let availableTask = this._tasks.find(t => t.isFree);
+            if (!availableTask) {
+                let task = yield Promise.race(this._tasks.map(t => t.promise));
+                task.free();
+                availableTask = task;
+            }
+            availableTask.execute(item, itemIndex);
+        });
+    }
+    finish() {
+        return Promise.all(this._tasks.filter(t => !t.isFree).map(t => t.promise));
+    }
+}
+class Task {
+    constructor(manager, id, taskFunc, captureResult) {
+        this._manager = manager;
+        this._id = id;
+        this._taskFunc = taskFunc;
+        this._captureResult = captureResult;
+        this._promise = null;
+    }
+    get promise() { return this._promise; }
+    get isFree() { return this._promise === null; }
+    execute(item, itemIndex) {
+        this._promise = new Promise((resolve, reject) => {
+            this._taskFunc(item)
+                .then((result) => {
+                if (this._captureResult)
+                    this._manager.addResult(itemIndex, result);
+                resolve(this);
+            })
+                .catch((err) => reject(err));
+        });
+    }
+    free() {
+        this._promise = null;
+    }
+}
 //# sourceMappingURL=make.js.map
