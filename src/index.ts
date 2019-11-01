b95f36c5c6a3654387a98b88e72b88d61d3d3128
nivinjoseph <nivinjoseph@outlook.com>
2019-11-01 03:35:42 -0400

    implemented semver utility class

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,14 @@ import { Time } from "./time";
 import { ImageHelper } from "./image-helper";
 import { Deferred } from "./deferred";
 import { Mutex } from "./mutex";
+import { Version } from "./version";
 import { Profiler, ProfilerTrace } from "./profiler";
 import { Templator } from "./templator";
 
 
 export
 {
-    Make, Delay, Disposable, BackgroundProcessor, Uuid, TypeHelper, Duration, Time, ImageHelper, Deferred, Mutex,
+    Make, Delay, Disposable, BackgroundProcessor, Uuid, TypeHelper, Duration, Time, ImageHelper, Deferred, Mutex, Version,
     
     Profiler, ProfilerTrace,
     
---
+++ src/version.ts
@@ -0,0 +1,85 @@
+import { given } from "@nivinjoseph/n-defensive";
+import { ArgumentException, InvalidArgumentException } from "@nivinjoseph/n-exception";
+import { TypeHelper } from "./type-helper";
+
+
+export class Version
+{
+    private readonly _major: number;
+    private readonly _minor: number;
+    private readonly _patch: number;
+    
+    
+    public get major(): number { return this._major; }
+    public get minor(): number { return this._minor; }
+    public get patch(): number { return this._patch; }
+    
+    public get full(): string { return `${this._major}.${this._minor}.${this._patch}`; }
+    
+    
+    public constructor(semanticVersion: string)
+    {
+        given(semanticVersion, "semanticVersion").ensureHasValue().ensureIsString();
+        semanticVersion = semanticVersion.trim();
+        
+        const split = semanticVersion.split(".");
+        if (split.length !== 3)
+            throw new InvalidArgumentException("semanticVersion");
+        
+        const major = TypeHelper.parseNumber(split[0]);
+        if (major == null)
+            throw new ArgumentException("semanticVersion", "invalid major");
+        this._major = major;
+        
+        const minor = TypeHelper.parseNumber(split[1]);
+        if (minor == null)
+            throw new ArgumentException("semanticVersion", "invalid minor");
+        this._minor = minor;
+        
+        const patch = TypeHelper.parseNumber(split[2]);
+        if (patch == null)
+            throw new ArgumentException("semanticVersion", "invalid patch");
+        this._patch = patch;
+    }
+    
+    
+    public equals(version: Version): boolean
+    {
+        given(version, "version").ensureHasValue().ensureIsObject().ensureIsInstanceOf(Version);
+        
+        return version.major === this.major && version.minor === this.minor && version.patch === this.patch;
+    }
+    
+    public compareTo(version: Version): number
+    {
+        given(version, "version").ensureHasValue().ensureIsObject().ensureIsInstanceOf(Version);
+        
+        const majorCompare = this.compare(this.major, version.major);
+        if (majorCompare !== 0)
+            return majorCompare;
+        
+        const minorCompare = this.compare(this.minor, version.minor);
+        if (minorCompare !== 0)
+            return minorCompare;
+        
+        return this.compare(this.patch, version.patch);
+    }
+    
+    public toString(): string
+    {
+        return this.full;
+    }
+    
+    private compare(v1: number, v2: number): number
+    {
+        given(v1, "v1").ensureHasValue().ensureIsNumber();
+        given(v2, "v2").ensureHasValue().ensureIsNumber();
+        
+        if (v1 > v2)
+            return 1;
+        if (v1 < v2)
+            return -1;
+        else
+            return 0;
+    }
+}
