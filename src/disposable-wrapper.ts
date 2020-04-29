bab7f02895a27ef33e8e0edf462a70a6f6b81a0d
nivinjoseph <nivinjoseph@outlook.com>
2020-04-28 23:24:09 -0400

    added disposable-wrapper

---
+++ src/disposable-wrapper.ts
@@ -0,0 +1,29 @@
+import { given } from "@nivinjoseph/n-defensive";
+import { Disposable } from "./disposable";
+
+// public
+export class DisposableWrapper implements Disposable
+{
+    private readonly _disposeFunc: () => Promise<void>;
+    private _isDisposed = false;
+    private _disposePromise: Promise<void> | null = null;
+    
+    
+    public constructor(disposeFunc: () => Promise<void>)
+    {
+        given(disposeFunc, "disposeFunc").ensureHasValue().ensureIsFunction();
+        this._disposeFunc = disposeFunc;
+    }
+    
+    
+    public dispose(): Promise<void>
+    {
+        if (!this._isDisposed)
+        {
+            this._isDisposed = true;
+            this._disposePromise = this._disposeFunc();
+        }
+        
+        return this._disposePromise;
+    }
+}
