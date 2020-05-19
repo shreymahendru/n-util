ec6d7e2fefd2bbc9eee87934348fbd10c2c99e06
nivinjoseph <nivinjoseph@outlook.com>
2020-05-18 21:38:47 -0400

    preparing to publish new version

--- dist/dto-factory.d.ts
+++ dist/dto-factory.d.ts
@@ -1,6 +1,6 @@
 export declare class DtoFactory {
     private constructor();
-    static create<T extends object>(value: T, keys: Array<keyof T | {
+    static create<T extends object, TDto extends {} = {}>(value: T, keys: Array<keyof T | {
         [key: string]: keyof T | ((val: T) => any);
-    }>): Partial<T>;
+    }>): TDto;
 }
