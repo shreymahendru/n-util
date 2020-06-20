d2aae6cf9e990c7e67c0eb10283f177556d9752b
nivinjoseph <nivinjoseph@outlook.com>
2020-06-20 00:34:55 -0400

    added PartialPick utility type

--- src/utility-types.ts
+++ src/utility-types.ts
@@ -1,4 +1,11 @@
 /**
+ * From T, pick a set of properties as optional whose keys are in the union K
+ */
+export type PartialPick < T, K extends keyof T > = {
+    [P in K]?: T[P];
+};
+
+/**
  * From T, pick a set of properties whose keys are in the union K
  */
 export type Schema<T, K extends keyof T> = {
