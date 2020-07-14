b88229c0b3fff0a7a43e08bbe8a0fbf311a1dd21
nivinjoseph <nivinjoseph@outlook.com>
2020-07-14 06:33:29 -0400

    preparing to publish new version

--- dist/serializable.js
+++ dist/serializable.js
@@ -43,77 +43,74 @@ class Serializable {
     }
 }
 exports.Serializable = Serializable;
-let Deserializer = (() => {
-    class Deserializer {
-        constructor() { }
-        static hasType(typeName) {
-            if (typeName == null || typeof typeName !== "string" || typeName.isEmptyOrWhiteSpace()
-                || !this._typeCache.has(typeName.trim()))
-                return false;
-            return true;
-        }
-        static registerType(type) {
-            n_defensive_1.given(type, "type").ensureHasValue();
-            const typeName = type.getTypeName();
-            if (!this._typeCache.has(typeName))
-                this._typeCache.set(typeName, type);
-        }
-        static deserialize(serialized) {
-            n_defensive_1.given(serialized, "serialized").ensureHasValue().ensureIsObject()
-                .ensure(t => t.$typename && typeof t.$typename === "string"
-                && !t.$typename.isEmptyOrWhiteSpace(), "$typename property is missing on object");
-            const typeName = serialized.$typename;
-            let type = Deserializer.getType(typeName);
-            if (type == null)
-                throw new n_exception_1.ApplicationException(`Cannot deserialize unregistered type '${typeName}'.`);
-            if (typeof type === "object")
-                type = type.constructor;
-            if (type.deserialize && typeof type.deserialize === "function")
-                return type.deserialize(serialized);
-            serialized = Object.keys(serialized).reduce((acc, key) => {
-                const val = serialized[key];
-                if (val == null) {
-                    acc[key] = null;
-                    return acc;
-                }
-                if (typeof val === "object") {
-                    if (Array.isArray(val)) {
-                        acc[key] = val.map((v) => {
-                            if (v == null)
-                                return null;
-                            if (typeof v === "object") {
-                                if (v.$typename && typeof v.$typename === "string" && !v.$typename.isEmptyOrWhiteSpace())
-                                    return Deserializer.deserialize(v);
-                            }
-                            return v;
-                        });
-                    }
-                    else {
-                        if (val.$typename && typeof val.$typename === "string" && !val.$typename.isEmptyOrWhiteSpace())
-                            acc[key] = Deserializer.deserialize(val);
-                        else
-                            acc[key] = val;
-                    }
+class Deserializer {
+    constructor() { }
+    static hasType(typeName) {
+        if (typeName == null || typeof typeName !== "string" || typeName.isEmptyOrWhiteSpace()
+            || !this._typeCache.has(typeName.trim()))
+            return false;
+        return true;
+    }
+    static registerType(type) {
+        n_defensive_1.given(type, "type").ensureHasValue();
+        const typeName = type.getTypeName();
+        if (!this._typeCache.has(typeName))
+            this._typeCache.set(typeName, type);
+    }
+    static deserialize(serialized) {
+        n_defensive_1.given(serialized, "serialized").ensureHasValue().ensureIsObject()
+            .ensure(t => t.$typename && typeof t.$typename === "string"
+            && !t.$typename.isEmptyOrWhiteSpace(), "$typename property is missing on object");
+        const typeName = serialized.$typename;
+        let type = Deserializer.getType(typeName);
+        if (type == null)
+            throw new n_exception_1.ApplicationException(`Cannot deserialize unregistered type '${typeName}'.`);
+        if (typeof type === "object")
+            type = type.constructor;
+        if (type.deserialize && typeof type.deserialize === "function")
+            return type.deserialize(serialized);
+        serialized = Object.keys(serialized).reduce((acc, key) => {
+            const val = serialized[key];
+            if (val == null) {
+                acc[key] = null;
+                return acc;
+            }
+            if (typeof val === "object") {
+                if (Array.isArray(val)) {
+                    acc[key] = val.map((v) => {
+                        if (v == null)
+                            return null;
+                        if (typeof v === "object") {
+                            if (v.$typename && typeof v.$typename === "string" && !v.$typename.isEmptyOrWhiteSpace())
+                                return Deserializer.deserialize(v);
+                        }
+                        return v;
+                    });
                 }
                 else {
-                    acc[key] = val;
+                    if (val.$typename && typeof val.$typename === "string" && !val.$typename.isEmptyOrWhiteSpace())
+                        acc[key] = Deserializer.deserialize(val);
+                    else
+                        acc[key] = val;
                 }
-                return acc;
-            }, {});
-            return new type(serialized);
-        }
-        static getType(typeName) {
-            n_defensive_1.given(typeName, "typeName").ensureHasValue().ensureIsString();
-            typeName = typeName.trim();
-            if (this._typeCache.has(typeName))
-                return this._typeCache.get(typeName);
-            return null;
-        }
+            }
+            else {
+                acc[key] = val;
+            }
+            return acc;
+        }, {});
+        return new type(serialized);
     }
-    Deserializer._typeCache = new Map();
-    return Deserializer;
-})();
+    static getType(typeName) {
+        n_defensive_1.given(typeName, "typeName").ensureHasValue().ensureIsString();
+        typeName = typeName.trim();
+        if (this._typeCache.has(typeName))
+            return this._typeCache.get(typeName);
+        return null;
+    }
+}
 exports.Deserializer = Deserializer;
+Deserializer._typeCache = new Map();
 function serialize(keyOrTarget, propertyKey, descriptor) {
     if (keyOrTarget != null && typeof keyOrTarget === "object") {
         const target = keyOrTarget;
@@ -145,46 +142,43 @@ function deserialize(target) {
     Deserializer.registerType(target);
 }
 exports.deserialize = deserialize;
-let Utilities = (() => {
-    class Utilities {
-        static getPropertyInfos(val, typeName) {
-            n_defensive_1.given(val, "val").ensureHasValue().ensureIsObject();
-            n_defensive_1.given(typeName, "typeName").ensureHasValue().ensureIsString();
-            if (!Utilities.typeCache.has(typeName))
-                Utilities.typeCache.set(typeName, Utilities.getPropertyInfosInternal(val));
-            return Utilities.typeCache.get(typeName);
-        }
-        static getPropertyInfosInternal(val) {
-            var _a;
-            let propertyInfos = new Array();
-            let prototype = Object.getPrototypeOf(val);
-            if (prototype === undefined || prototype === null)
-                return propertyInfos;
-            propertyInfos.push(...Utilities.getPropertyInfosInternal(prototype));
-            let propertyNames = Object.getOwnPropertyNames(val);
-            for (let name of propertyNames) {
-                name = name.trim();
-                if (name === "constructor" || name.startsWith("_") || name.startsWith("$") || Utilities.internal.some(t => t === name))
-                    continue;
-                if (Utilities.forbidden.some(t => t === name))
-                    throw new n_exception_1.ApplicationException(`Class ${val.getTypeName()} has a member with the forbidden name '${name}'. The following names are forbidden: ${Utilities.forbidden}.`);
-                const descriptor = Object.getOwnPropertyDescriptor(val, name);
-                if (descriptor.get && descriptor.get.serializable) {
-                    propertyInfos.push({
-                        name,
-                        descriptor,
-                        serializationKey: (_a = descriptor.get.serializationKey) !== null && _a !== void 0 ? _a : name
-                    });
-                }
-            }
-            return propertyInfos;
-        }
+class Utilities {
+    static getPropertyInfos(val, typeName) {
+        n_defensive_1.given(val, "val").ensureHasValue().ensureIsObject();
+        n_defensive_1.given(typeName, "typeName").ensureHasValue().ensureIsString();
+        if (!Utilities.typeCache.has(typeName))
+            Utilities.typeCache.set(typeName, Utilities.getPropertyInfosInternal(val));
+        return Utilities.typeCache.get(typeName);
     }
-    Utilities.typeCache = new Map();
-    Utilities.internal = [];
-    Utilities.forbidden = ["do", "if", "for", "let", "new", "try", "var", "case", "else", "with", "await", "break",
-        "catch", "class", "const", "super", "throw", "while", "yield", "delete", "export", "import", "return",
-        "switch", "default", "extends", "finally", "continue", "debugger", "function", "arguments", "typeof", "void"];
-    return Utilities;
-})();
+    static getPropertyInfosInternal(val) {
+        var _a;
+        let propertyInfos = new Array();
+        let prototype = Object.getPrototypeOf(val);
+        if (prototype === undefined || prototype === null)
+            return propertyInfos;
+        propertyInfos.push(...Utilities.getPropertyInfosInternal(prototype));
+        let propertyNames = Object.getOwnPropertyNames(val);
+        for (let name of propertyNames) {
+            name = name.trim();
+            if (name === "constructor" || name.startsWith("_") || name.startsWith("$") || Utilities.internal.some(t => t === name))
+                continue;
+            if (Utilities.forbidden.some(t => t === name))
+                throw new n_exception_1.ApplicationException(`Class ${val.getTypeName()} has a member with the forbidden name '${name}'. The following names are forbidden: ${Utilities.forbidden}.`);
+            const descriptor = Object.getOwnPropertyDescriptor(val, name);
+            if (descriptor.get && descriptor.get.serializable) {
+                propertyInfos.push({
+                    name,
+                    descriptor,
+                    serializationKey: (_a = descriptor.get.serializationKey) !== null && _a !== void 0 ? _a : name
+                });
+            }
+        }
+        return propertyInfos;
+    }
+}
+Utilities.typeCache = new Map();
+Utilities.internal = [];
+Utilities.forbidden = ["do", "if", "for", "let", "new", "try", "var", "case", "else", "with", "await", "break",
+    "catch", "class", "const", "super", "throw", "while", "yield", "delete", "export", "import", "return",
+    "switch", "default", "extends", "finally", "continue", "debugger", "function", "arguments", "typeof", "void"];
 //# sourceMappingURL=serializable.js.map
