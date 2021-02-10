3a81bf5635b88e3cdd485d1f07e7e81db469ac62
nivinjoseph <nivinjoseph@outlook.com>
2021-02-10 10:23:01 -0500

    preparing to publish new version

--- dist/utility-types.d.ts
+++ dist/utility-types.d.ts
@@ -7,3 +7,7 @@ export declare type Schema<T, K extends keyof T> = {
 export declare type PartialSchema<T, K extends keyof T> = {
     -readonly [P in K]?: T[P];
 };
+export declare type ClassDefinition<T extends {}> = new (...args: any[]) => T;
+export declare type ClassHierarchy<T extends {}> = Function & {
+    prototype: T;
+};
