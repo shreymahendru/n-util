dabba369800dcbc46192e3c680f6a4c2146ed581
nivinjoseph <nivinjoseph@outlook.com>
2021-06-26 13:16:06 -0400

    implemented observer

---
+++ src/observer.ts
@@ -0,0 +1,54 @@
+import { given } from "@nivinjoseph/n-defensive";
+
+
+export class Observer<T>
+{
+    private readonly _event: string;
+    private _callback: (eventData: T) => void;
+    private readonly _subscription: Subscription;
+
+
+    public get event(): string { return this._event; }
+    public get subscription(): Subscription { return this._subscription; }
+    public get isCancelled(): boolean { return this._callback == null; }
+
+
+    public constructor(event: string, callback: (eventData: T) => void)
+    {
+        given(event, "event").ensureHasValue().ensureIsString();
+        this._event = event.trim();
+
+        given(callback, "callback").ensureHasValue().ensureIsFunction();
+        this._callback = callback;
+
+        this._subscription = {
+            event: this._event,
+            isUnsubscribed: false,
+            unsubscribe: () => this.cancel()
+        };
+    }
+
+    public notify(eventData: T): void
+    {
+        // no defensive check cuz eventData can be void
+
+        if (this.isCancelled)
+            return;
+        
+        setTimeout(this._callback, 0, eventData);
+    }
+
+    public cancel(): void
+    {
+        this._callback = null;
+        (<any>this._subscription).isUnsubscribed = true;
+    }
+}
+
+
+export interface Subscription
+{
+    readonly event: string;
+    readonly isUnsubscribed: boolean;
+    unsubscribe(): void;
+}
