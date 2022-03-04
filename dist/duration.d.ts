a27e77b4bf1ac8c58ce95190a59747a937302142
nivinjoseph <nivinjoseph@outlook.com>
2022-03-04 00:25:10 -0500

    preparing to publish new version

--- dist/duration.d.ts
+++ dist/duration.d.ts
@@ -1,10 +1,16 @@
-/**
- * @static
- */
 export declare class Duration {
+    private readonly _ms;
     private constructor();
-    static fromSeconds(seconds: number): number;
-    static fromMinutes(minutes: number): number;
-    static fromHours(hours: number): number;
-    static fromDays(days: number): number;
+    static fromMilliSeconds(milliSeconds: number): Duration;
+    static fromSeconds(seconds: number): Duration;
+    static fromMinutes(minutes: number): Duration;
+    static fromHours(hours: number): Duration;
+    static fromDays(days: number): Duration;
+    static fromWeeks(weeks: number): Duration;
+    toMilliSeconds(round?: boolean): number;
+    toSeconds(round?: boolean): number;
+    toMinutes(round?: boolean): number;
+    toHours(round?: boolean): number;
+    toDays(round?: boolean): number;
+    toWeeks(round?: boolean): number;
 }
--- dist/duration.js
+++ dist/duration.js
@@ -2,14 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Duration = void 0;
 const n_defensive_1 = require("@nivinjoseph/n-defensive");
-/**
- * @static
- */
 class Duration {
-    constructor() { }
+    constructor(ms) {
+        n_defensive_1.given(ms, "ms").ensureHasValue().ensureIsNumber().ensure(t => t >= 0);
+        this._ms = ms;
+    }
+    static fromMilliSeconds(milliSeconds) {
+        return new Duration(milliSeconds);
+    }
     static fromSeconds(seconds) {
         n_defensive_1.given(seconds, "seconds").ensureHasValue().ensureIsNumber();
-        return seconds * 1000;
+        return this.fromMilliSeconds(seconds * 1000);
     }
     static fromMinutes(minutes) {
         n_defensive_1.given(minutes, "minutes").ensureHasValue().ensureIsNumber();
@@ -22,6 +25,34 @@ class Duration {
     static fromDays(days) {
         n_defensive_1.given(days, "days").ensureHasValue().ensureIsNumber();
         return this.fromHours(days * 24);
+    }
+    static fromWeeks(weeks) {
+        n_defensive_1.given(weeks, "weeks").ensureHasValue().ensureIsNumber();
+        return this.fromDays(weeks * 7);
+    }
+    toMilliSeconds(round = false) {
+        const result = this._ms;
+        return round ? Math.round(result) : result;
+    }
+    toSeconds(round = false) {
+        const result = this.toMilliSeconds() / 1000;
+        return round ? Math.round(result) : result;
+    }
+    toMinutes(round = false) {
+        const result = this.toSeconds() / 60;
+        return round ? Math.round(result) : result;
+    }
+    toHours(round = false) {
+        const result = this.toMinutes() / 60;
+        return round ? Math.round(result) : result;
+    }
+    toDays(round = false) {
+        const result = this.toHours() / 24;
+        return round ? Math.round(result) : result;
+    }
+    toWeeks(round = false) {
+        const result = this.toDays() / 7;
+        return round ? Math.round(result) : result;
     }
 }
 exports.Duration = Duration;
